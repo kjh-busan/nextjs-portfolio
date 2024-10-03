eeb24f6e2827131fcfbccca9184e9dddd548d336
김주환 <gimjuhwan@gimjuhwan-ui-Macmini.local>
2024-10-04 00:15:28 +0900

    feat: convert korean to japanese

--- pages/projects.js
+++ pages/projects.js
@@ -3,31 +3,32 @@ import Head from "next/head";
 import { TOKEN, DATABASE_ID } from "../config";
 import ProjectItem from "../components/projects/project-item";
 
-export default function Projects({projects}) {
+export default function Projects({ projects }) {
+  console.log(projects);
 
-    console.log(projects);
+  return (
+    <Layout>
+      <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
+        <Head>
+          <title>KIM JOOHWANポートフォリオ</title>
+          <meta name="description" content="오늘도 빡코딩!" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1 className="text-4xl font-bold sm:text-6xl">
+          現場経験 :
+          <span className="pl-4 text-blue-500">
+            {projects.results.length}箇所
+          </span>
+        </h1>
 
-    return (
-        <Layout >
-            <div className="flex flex-col items-center justify-center min-h-screen px-3 mb-10">
-                <Head>
-                    <title>우리가족들의 추억</title>
-                    <meta name="description" content="오늘도 빡코딩!" />
-                    <link rel="icon" href="/favicon.ico" />
-                </Head>
-                <h1 className="text-4xl font-bold sm:text-6xl">
-                업데이트된 총 구성원 :
-                    <span className="pl-4 text-blue-500">{projects.results.length}명</span>
-                </h1>
-
-                <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
-                    {projects.results.map((aProject) => (
-                        <ProjectItem key={aProject.id} data={aProject}/>
-                    ))}
-                </div>
-            </div>
-        </Layout>
-    );
+        <div className="grid grid-cols-1 gap-8 p-12 m-4 md:grid-cols-2">
+          {projects.results.map((aProject) => (
+            <ProjectItem key={aProject.id} data={aProject} />
+          ))}
+        </div>
+      </div>
+    </Layout>
+  );
 }
 
 // 빌드 타임에 호출
@@ -35,37 +36,39 @@ export default function Projects({projects}) {
 
 // 각 요청 때마다 호출
 export async function getServerSideProps() {
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Notion-Version": "2022-02-22",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${TOKEN}`,
+    },
+    body: JSON.stringify({
+      sorts: [
+        {
+          property: "lifeperiod",
+          direction: "ascending",
+        },
+      ],
+      page_size: 100,
+    }),
+  };
 
-	const options = {
-		method: 'POST',
-		headers: {
-			Accept: 'application/json',
-			'Notion-Version': '2022-02-22',
-			'Content-Type': 'application/json',
-			Authorization: `Bearer ${TOKEN}`
-		},
-		body: JSON.stringify({
-			sorts: [
-				{
-					"property": "lifeperiod",
-					"direction": "ascending"
-				}
-			],
-			page_size: 100
-		})
-	};
+  const res = await fetch(
+    `https://api.notion.com/v1/databases/${DATABASE_ID}/query`,
+    options
+  );
+  const projects = await res.json();
+  const projectNames = projects.results.map(
+    (aProject) => aProject.properties.Name.title[0].plain_text
+  );
 
-	const res = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, options);
-	const projects = await res.json();
-	const projectNames = projects.results.map((aProject) =>(
-		aProject.properties.Name.title[0].plain_text
-	))
+  console.log(`projectNames : ${projectNames}`);
 
-	console.log(`projectNames : ${projectNames}`);
-
-	return {
-		props: {projects}, // will be passed to the page component as props
-		// getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
-		// revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
-	}
+  return {
+    props: { projects }, // will be passed to the page component as props
+    // getStaticProps() 메소드를 사용한다면 revalidate 로 데이터 변경시 갱신가능!
+    // revalidate: 1 // 데이터 변경이 있으면 갱신 1초 마다
+  };
 }
